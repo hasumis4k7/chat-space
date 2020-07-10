06af699c7c11d4d731de42632c355f586ab47b4f
hasumis4k7 <hsm.bb8@ezweb.ne.jp>
2020-07-10 12:04:00 +0900

    initial bommit

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if (message.image) {
       let html = 
-      `<div class = "main-chat__message-list__box">
+      `<div class = "main-chat__message-list__box" data-message-id=${message.id}>
         <div class = "main-chat__message-list__box__code">
           <div class = "main-chat__message-list__box__code__user">
             ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class = "main-chat__message-list__box">
+      `<div class = "main-chat__message-list__box" data-message-id=${message.id}>
         <div class = "main-chat__message-list__box__code">
           <div class = "main-chat__message-list__box__code__user">
             ${message.user_name}
@@ -42,27 +42,32 @@ $(function(){
     
 
 
-  $('.Form').on('submit', function(e){
-    e.preventDefault()
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+  let reloadMessages = function() {
+    let last_message_id = $('.MessageBox:last').data("message-id");
     $.ajax({
-      url: url,  //同期通信でいう『パス』
-      type: 'POST',  //同期通信でいう『HTTPメソッド』
-      data: formData,  
-      dataType: 'json',
-      processData: false,
-      contentType: false
+      url: "api/messages",  //同期通信でいう『パス』
+      type: 'get',  //同期通信でいう『HTTPメソッド』
+      data: {id: last_message_id},  
+      dataType: 'json'
     })
-    .done(function(post){
-      let html = buildHTML(post);
-      $('.main-chat__message-list').append(html);
-      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
-      $('form')[0].reset();
-      $('input').prop('disabled', false)
+    .done(function(messages){
+      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
+      alert("error");
     });
-  })
+  };
+  setInterval(reloadMessages, 7000);
+
 });
